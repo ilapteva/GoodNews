80a8b1f90bcb6eb25f7201ced46185d5be4e455f
Irina Lapteva <iralapteva44@gmail.com>
2020-07-26 13:14:06 +0300

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Ира on 26.07.2020.
+//  Copyright © 2020 Irina Lapteva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
